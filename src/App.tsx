4bb1b95d309202c02de9de5cc88c733972701be6
Adil <Adil>
2023-09-25 16:53:05 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Character from './component/Character/character';
+import Calculation from './component/Сalculation/calculation';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculation />
+      <div className="ocean">
+      <div className="oceanUseable">    
+          <Character />
+      </div>
+      <div className="oceanUnUseable"></div>
+      </div>
+     
+  
     </div>
   );
 }
